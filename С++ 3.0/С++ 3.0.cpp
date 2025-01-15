2a1191b53defe23c6305c207306c8bf207a46199
OLXza_fu90 <OLXza_fu90@13-4>
2025-01-15 18:24:46 +0200

    Add project files.

---
+++ С++ 3.0/С++ 3.0.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class People {
+    string name;
+    string surname;
+    string fathername;
+    int year;
+public:
+    People() {
+        this->name = "Kolya";
+        this->surname = "Yukibovich";
+        this->fathername = "Kostyntinovich";
+        this->year = 16;
+    }
+    void Print() {
+        cout << "Name: " << name << endl << "Surname: " << surname << endl << "Fathername: " << fathername << endl << "Year: " << year << endl;
+    }
+};
+
+int main()
+{
+    unique_ptr<People[]> ptr1(new People[5]);
+
+    for (int i = 0; i < 5; i++) {
+        ptr1[i].Print();
+    }
+    cout << "----------------------" << endl;
+    shared_ptr<People[]> ptr2(new People[5]);
+    for (int i = 0; i < 5; i++) {
+        ptr2[i].Print();
+    }
+}
