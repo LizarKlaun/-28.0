11cf72bd534a9616b2d07bfd42c2d32ec36e9e2d
OLXza_fu90 <OLXza_fu90@13-4>
2025-01-15 19:34:48 +0200

    1234

--- С++ 3.0/С++ 3.0.cpp
+++ С++ 3.0/С++ 3.0.cpp
@@ -7,12 +7,22 @@ using namespace std;
 class Multi {
     char factor;
 public:
-    Multi() {
-    }
     explicit Multi(char factor) : factor(factor) {};
-    void operator()(int& elem) {
+    void operator()(int& elem) const {
         if(factor == '+') {
-
+            elem += elem;
+        }
+        else if (factor == '-') {
+            elem -= elem;
+        }
+        else if (factor == '*') {
+            elem *= elem;
+        }
+        else if (factor == '/') {
+            elem /= elem;
+        }
+        else if (factor == '%') {
+            elem = 0;
         }
     }
 };
@@ -20,7 +30,7 @@ public:
 int main()
 {
     vector<int> vec = {1,2,3,4};
-    for_each(vec.begin(), vec.end(), Multi(+));
+    for_each(vec.begin(), vec.end(), Multi('%'));
     for (const auto& elem : vec)
     {
         cout << elem << endl;
